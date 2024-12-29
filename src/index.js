0d547ce58b22be0d4531844b604a7519ce6d15e9
bpmdevr <bpolatmd@gmail.com>
2024-12-29 16:06:26 +0300

    first commit to the new repository

--- src/index.js
+++ src/index.js
@@ -10,11 +10,12 @@ import 'tachyons';
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
 
+// ReactDOM.render(<App />, document.getElementById('root'));
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 
 //serviceWorker.unregister();
 
-// ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
